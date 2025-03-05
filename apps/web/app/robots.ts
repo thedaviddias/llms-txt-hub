5d54ce2667171dcefe99219c91f6949f469a2f76
David Dias <code@thedaviddias.com>
2025-03-05 18:36:12 -0500

    fix: add more urls to robots

--- apps/web/app/robots.ts
+++ apps/web/app/robots.ts
@@ -5,7 +5,7 @@ export default function robots(): MetadataRoute.Robots {
     rules: {
       userAgent: '*',
       allow: ['/'],
-      disallow: ['/404', '/500', '/submit', '/api/*']
+      disallow: ['/404', '/500', '/submit', '/api/*', '/sort=*', '/search=*', '/category=*']
     }
   }
 }
